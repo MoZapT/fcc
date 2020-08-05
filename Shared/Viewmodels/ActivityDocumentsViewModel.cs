f0f6c5d9d28409c32df158f73ec05ad4c81c7c6c
Thomas Mayer <thomas.mayer@krause-systems.de>
2020-08-05 06:59:57 +0200

    new vm, some resources and db work

---
+++ Shared/Viewmodels/ActivityDocumentsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Shared.Common;
+using Shared.Models;
+
+namespace Shared.Viewmodels
+{
+    public class ActivityDocumentsViewModel : BaseViewModel
+    {
+
+        #region PROPERTIES
+
+        public Dictionary<string, IEnumerable<PersonDocument>> Documents { get; set; }
+
+        #endregion
+
+        public ActivityDocumentsViewModel()
+        {
+            Documents = new Dictionary<string, IEnumerable<PersonDocument>>();
+        }
+
+    }
+}
